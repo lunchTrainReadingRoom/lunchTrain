8e45a90d5789de46b4ec6a75c40e145a0a0d3212
jonathan.brooks <lunch.train.readingroom@gmail.com>
2016-11-04 17:06:59 +0000

    CORS fix

--- app.js
+++ app.js
@@ -14,12 +14,21 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+//CORS middleware
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'example.com');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(allowCrossDomain());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
