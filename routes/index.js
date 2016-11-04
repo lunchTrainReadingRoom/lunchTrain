9f33f653e172109170ef4c2d4a9190d5f3e68e4d
jonathan.brooks <lunch.train.readingroom@gmail.com>
2016-11-04 16:49:40 +0000

    Schema changes Added API endpoints

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,5 @@
 var express = require('express');
-var router = express.Router();
-var mongoose = require('mongoose');
-
-//connect to local mongodb database
-var db = mongoose.connect('mongodb://lunchTrain:readingroom@ds143717.mlab.com:43717/lunch_train_db1');
-var User = require('../models/user');
-  
-// create a new user called test
-var test = new User({
-  name: 'test',
-  username: 'lunchTrain',
-  password: 'readingroom' 
-});
-
-// call the built-in save method to save to the database
-test.save(function(err) {
-  if (err) throw err;
-  console.log('User saved successfully!');
-});
-
-//attach lister to connected event
-mongoose.connection.once('connected', function() {
-	console.log('mongoose connection established');
-});
+var router = express.Router(); 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
---
+++ routes/train.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+
+//connect to local mongodb database
+var db = mongoose.connect('mongodb://lunchTrain:readingroom@ds143717.mlab.com:43717/lunch_train_db1');
+var Train = require('../models/train');
+
+
+router.get('/add', function(req, res, next) {
+	// create a new user	
+	var train = new Train({
+	  conductor: req.body.conductor,
+	  destination: req.body.destination,
+	  time: req.body.time,
+	  notes: req.body.notes
+	});
+	// call the built-in save method to save to the database
+	train.save(function(err) {
+	  if (err) throw err;
+	  console.log('Train saved successfully!');
+	});
+});
+
+router.get('/getAll', function(req, res, next) {
+  Train.find({}, function(err, trains) {
+  	res.send(trains);
+  })
+});
+
+module.exports = router;
