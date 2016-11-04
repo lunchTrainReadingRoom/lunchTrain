99a6f0868569c1696b6775566829b56a8793b31d
jonathan.brooks <lunch.train.readingroom@gmail.com>
2016-11-04 17:20:12 +0000

    Added options fix

--- app.js
+++ app.js
@@ -14,10 +14,14 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.all('/*', function(req, res, next) {
+app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
+});
+
+app.options("*", function(req,res,next){
+  res.send(200);  
 });
 
 // uncomment after placing your favicon in /public
